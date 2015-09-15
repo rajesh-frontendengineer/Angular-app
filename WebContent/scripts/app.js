09bcdf4b4b54afcbd1c9c5d76de167760cf53f75
central-ui <veldandi.rajesh@gmail.com>
2015-09-14 23:02:55 -0700

    Config  file added

--- WebContent/scripts/app.js
+++ WebContent/scripts/app.js
@@ -1,3 +1,10 @@
 /**
- * Created by rajeshdarling on 9/10/2015.
+ * Created by rajesh veldandi on 9/10/2015.
  */
+
+"use strict";
+(function(){
+
+    var
+    angular.module('captain-ap'); //get module
+})();
--- WebContent/scripts/config.js
+++ WebContent/scripts/config.js
@@ -1,3 +1,39 @@
 /**
- * Created by rajeshdarling on 9/10/2015.
+ * Created by Rajesh Veldandi on 9/10/2015.
  */
+"use strict";
+(function(){
+
+    var captainApp = angular.module('captainApp'); //get module
+    angular.module('captainApp',['ui.router']);
+
+    angular.module('captainApp').config(['$stateProvider','$urlRouterProvider',moduleConfig]);
+
+    function moduleConfig($stateProvider, $urlRouterProvider) {
+        //
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/Error");
+        //
+        // Now set up the states
+        $stateProvider
+            //States and nested views
+            .state('Home-page', {
+                url: "/Home-page",
+                templateUrl: "views/Home-page.html"
+            })
+            .state('Category', {
+                url: "/Category",
+                templateUrl: "views/Category.html",
+                controllerAs: 'Ctgry'
+            })
+            .state('Category.Product-view', {
+                url: "/Product",// product name dynamic
+                templateUrl: "views/Product-view.html",
+                controller: ''
+            })
+
+
+};
+
+
+})();
